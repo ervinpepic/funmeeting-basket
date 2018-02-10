5b4b2400bef2e6d352528899809e84042d113ca6
Ervin Pepic <ervin.hack@gmail.com>
2018-02-11 00:04:52 +0100

    Added new Skill League Activity and added intents to change screen and back from start

--- app/src/main/java/com/example/ervinpepic/funmeeting_basket/WelcomActivity.kt
+++ app/src/main/java/com/example/ervinpepic/funmeeting_basket/WelcomActivity.kt
@@ -1,12 +1,19 @@
 package com.example.ervinpepic.funmeeting_basket
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_welcom.*
 
 class WelcomActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_welcom)
+
+        getstartedbutton.setOnClickListener {
+            val leagueIntent = Intent(this, LeagueActivity::class.java)
+            startActivity(leagueIntent)
+        }
     }
 }
